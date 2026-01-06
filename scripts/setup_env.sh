a907313db43ff1b7c7e0e888176928b5f4e5ed25
LCH1238 <lvchuanhao@foxmail.com>
2026-01-06 16:32:32 +0800

    [Feature]: add sm87, fix build error

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -5,8 +5,10 @@ set -e
 apt-get update && \
 apt-get install python3.8-dev python3-pip wget curl -y
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+python3 -m pip install --upgrade pip==25.0.1
 
-wget https://github.com/Kitware/CMake/releases/download/v3.27.9/cmake-3.27.9-linux-x86_64.tar.gz
-tar -xzf cmake-3.27.9-linux-x86_64.tar.gz
-mv cmake-3.27.9-linux-x86_64 /opt/cmake
+ARCH="$(uname -m)"
+wget https://github.com/Kitware/CMake/releases/download/v3.27.9/cmake-3.27.9-linux-$ARCH.tar.gz
+tar -xzf cmake-3.27.9-linux-$ARCH.tar.gz
+mv cmake-3.27.9-linux-$ARCH /opt/cmake
 ln -sf /opt/cmake/bin/cmake /usr/local/bin/cmake
